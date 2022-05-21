91a7eae6d67a05f4dfec6a789296535d622179cb
Jinay Shah <jinshah0322@gmail.com>
2022-05-21 11:48:29 +0530

    sum of n natural numbers

---
+++ Recurssion/sum_of_n_natural_numbers.py
@@ -0,0 +1,15 @@
+#find sum of n natural numbers
+def sum(n):
+    if(n==0):
+        return 0
+    elif(n==1):
+        return 1
+    else:
+        partialsum=sum(n-1)
+        return n+partialsum
+n=int(input())
+if(n<0):
+    print("enter valid number")
+else:
+    x=sum(n)
+    print(x)
