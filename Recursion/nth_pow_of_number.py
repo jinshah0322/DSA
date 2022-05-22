6c0c7f13d329b85d074cc26e27d732f646044ce2
Jinay Shah <jinshah0322@gmail.com>
2022-05-22 11:15:55 +0530

    solved with less space and time complexity than older

---
+++ Recursion/nth_pow_of_number.py
@@ -0,0 +1,17 @@
+#Practice - https://bit.ly/3FPCfV3
+import re
+
+
+def pow(x,n):
+    if(n==0):
+        return 1
+    temp=pow(x,n//2)
+    if(n%2==1):
+        return temp*temp*x
+    return temp*temp
+t=int(input())
+while t>0:
+    x,n=map(int,input().split())
+    ans=pow(x,n)
+    print(ans)
+    t-=1
