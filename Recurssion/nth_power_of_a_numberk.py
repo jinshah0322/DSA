013703f763d5ff23b8624337fd575a091869db68
Jinay Shah <jinshah0322@gmail.com>
2022-05-22 10:50:34 +0530

    modified answer

--- Recurssion/nth_power_of_a_numberk.py
+++ Recurssion/nth_power_of_a_numberk.py
@@ -1,11 +1,7 @@
 #Practice - https://bit.ly/3FPCfV3
 def pow(x,n):
-    if(x!=0 and n==0):
+    if(n==0 ):
         return 1
-    if(x==0 and n==0):
-        return 1
-    if(n==1):
-        return x
     else:
         mul=x*pow(x,n-1)
         return mul
