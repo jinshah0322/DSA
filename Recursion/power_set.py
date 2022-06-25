58c7bedec1225f9ea95334610df08a28c0a63b34
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2022-06-25 16:34:10 +0530

    power set

--- Recursion/power_set.py
+++ Recursion/power_set.py
@@ -2,12 +2,10 @@
 def pwsethelper():
     pass
 
-
 def pwset(v):
     # Write your code here
     # Return a 2-D list containing all subsets
     pass
-
 
 n = int(input())
 l = list(map(int, input().split()))
