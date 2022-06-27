4977e9f5a36d9c43a6df11933470371aecd95e24
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2022-06-27 15:59:54 +0530

    Start from front and end and print where booth meet(Done through both recursive as well as simple way) NOTE:simple way only works for odd numbers

---
+++ Codehef/HOOPS.py
@@ -0,0 +1,14 @@
+# cook your dish here
+# def fun(i,j):
+#     if(i==j):
+#         return i
+#     else:
+#         x=fun(i+1,j-1)
+#         return x
+    
+t=int(input())
+while(t>0):
+    n=int(input())
+    print((n+1)//2)
+    # print(fun(1,n))
+    t-=1
